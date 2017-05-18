5c9ebc93bb74063b42cd0e4c58f46b382c5f56cd
nilesh-cutinha <nilesh_joseph@hotmail.com>
2017-05-18 21:02:34 +0930

    Added java docs and removed unwanted imports

--- src/org/Interview/MeasuresOfLength/SortingMeasuresOfLengthMain.java
+++ src/org/Interview/MeasuresOfLength/SortingMeasuresOfLengthMain.java
@@ -1,15 +1,16 @@
 package org.Interview.MeasuresOfLength;
 
+/**
+ * The main class to test the working of Sorting Length Measures
+ */
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class SortingMeasuresOfLengthMain {
 
 	public static void main(String[] args) throws IOException {
-	    List<String> stringList = (List) Arrays.asList("1m", "1.5m", "10m", "2m", "2.5m", "3m","0m");
+	    List<String> stringList = (List<String>) Arrays.asList("1m", "1.5m", "10m", "2m", "2.5m", "3m","0m");
 	    SortMeasuresOfLength sortMeasuresOfLength = new SortMeasuresOfLength();
 	    stringList = sortMeasuresOfLength.sortUnsortedMeasuresOfLength(stringList);
 	    System.out.println(stringList);
