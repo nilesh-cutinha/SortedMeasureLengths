34de45406cda69494a193f532240aeba1291aaba
nilesh-cutinha <nilesh_joseph@hotmail.com>
2017-05-18 20:57:29 +0930

    Removed commented code

--- src/org/Interview/MeasuresOfLength/SortingMeasuresOfLengthMain.java
+++ src/org/Interview/MeasuresOfLength/SortingMeasuresOfLengthMain.java
@@ -10,7 +10,6 @@ public class SortingMeasuresOfLengthMain {
 
 	public static void main(String[] args) throws IOException {
 	    List<String> stringList = (List) Arrays.asList("1m", "1.5m", "10m", "2m", "2.5m", "3m","0m");
-		// List<String> stringList = (List) Arrays.asList("1.512mm","0.5mm","27mm", "20mm");
 	    SortMeasuresOfLength sortMeasuresOfLength = new SortMeasuresOfLength();
 	    stringList = sortMeasuresOfLength.sortUnsortedMeasuresOfLength(stringList);
 	    System.out.println(stringList);
